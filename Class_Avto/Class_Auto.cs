5aeb9571c88319b83621f16b8c01e626c7ea8cc8
Mamadzie <Mamad2005@bk.ru>
2023-12-26 10:39:05 +0300

    Добавьте файлы проекта.

---
+++ Class_Avto/Class_Auto.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Avto
+{
+    internal class Class_Auto
+    {
+        private string number;
+        private double topliva;
+        private double rasxod;
+        private double Tekushee_rasstoyanie = 0; // для текущего пройденного пути
+
+        public Class_Auto(string number, double topliva, double rasxod)
+        {
+            this.number = number;
+            this.topliva = topliva;
+            this.rasxod = rasxod;
+        }
+
+        public void info()
+        {
+            Console.WriteLine("Номер: " + number);
+            Console.WriteLine("Топливо: " + topliva);
+            Console.WriteLine("Расход: " + rasxod);
+        }
+
+
+        public void Zapravka(double top)
+        {
+            if (top > 0)
+            {
+                this.topliva += top;
+                Console.WriteLine("Топливо успешно заправлено. Текущий бак: " + (this.topliva += top) + " л");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Нельзя заправить отрицательное количество топлива.");
+            }
+        }
+
+
+        public void Proidenei_put(double rasstoyanie)
+        {
+            this.Tekushee_rasstoyanie += rasstoyanie;
+        }
+
+        public void Otobrojenie_rasstoyanie()
+        {
+            Console.WriteLine("Текущий пробег: " + this.Tekushee_rasstoyanie + " км");
+        }
+
+        private void Move(int km)
+        {
+            double temp_Flow = this.rasxod;
+            double rasstoyanie = km;
+
+           double Ostavshie_topliva = this.topliva - (rasstoyanie * (this.rasxod) / 100);
+
+            if (Ostavshie_topliva >= 0)
+            {
+                Console.WriteLine("Вы доехали!");
+                this.Tekushee_rasstoyanie += rasstoyanie;
+
+                // Обновляем оставшееся топливо
+                this.topliva = Ostavshie_topliva;
+            }
+            else
+            {
+                Console.WriteLine("Если не хотите дальше пешком, введите, сколько литров вы хотите заправить: ");
+                Zapravka(double.Parse(Console.ReadLine()));
+                Ostavshie_topliva = this.topliva - (rasstoyanie * (this.rasxod) / 100);
+
+                if (Ostavshie_topliva >= 0)
+                {
+                    Console.WriteLine("Вы смогли доехать до места назначения и в баке осталось: " + Ostavshie_topliva);
+                }
+                else
+                {
+                    double trebuyemaya_topliva = Math.Abs(Ostavshie_topliva);
+                    Console.WriteLine("Вам не хватило топлива до места назначения. Вам нужно было заправить ещё  " + trebuyemaya_topliva + " литров");
+                    this.Tekushee_rasstoyanie -= rasstoyanie;
+                }
+            }
+            // Обновляем оставшееся топливо
+            double update = this.topliva - (rasstoyanie * (this.rasxod) / 100);
+            Console.WriteLine("Текущий топлива: " + update);
+
+            this.Tekushee_rasstoyanie += rasstoyanie;
+            if (update <= 0)
+            {
+                Console.WriteLine("Путишествие закончено.Всего Хорошего");
+                Environment.Exit(0);
+                Console.ReadKey();
+            }
+            
+        }
+
+
+        public void MOVE(int km)
+        {
+            Move(km);
+        }
+
+        public double Get_obshee_rasstoyanie()
+        {
+            return this.Tekushee_rasstoyanie;
+        }
+    }
+}
---
+++ Class_Avto/Program.cs
@@ -0,0 +1,123 @@
+﻿using Class_Avto;
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double topliva;
+        Console.WriteLine("Введите количество топлива в баке до(80): ");
+        double.TryParse(Console.ReadLine(), out topliva);
+
+        while (topliva <= 0 || topliva > 80)
+        {
+            Console.WriteLine("Введите заново(больше 80 нельзя)");
+            double.TryParse(Console.ReadLine(), out topliva);
+        }
+
+        double topliva_rasxod;
+        Console.WriteLine("Введите расход топлива на 100км:");
+        double.TryParse(Console.ReadLine(), out topliva_rasxod);
+
+        while (topliva_rasxod <= 0 || topliva_rasxod > 100)
+        {
+            Console.WriteLine("Введите заново(больше 100 нельзя)");
+            double.TryParse(Console.ReadLine(), out topliva_rasxod);
+        }
+        Class_Auto[] m = new Class_Auto[3];
+
+        Console.WriteLine("  Выберите машину: ");
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine("1.Mers-Benz");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("2.BMW");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("3.Трактор");
+        Console.ForegroundColor = ConsoleColor.White;
+
+        int vibor = Convert.ToInt32(Console.ReadLine());
+        int index = 0;
+        switch (vibor)
+        {
+            case 1:
+                m[0] = new Class_Auto("777", topliva, topliva_rasxod);
+                index = 0;
+                break;
+            case 2:
+                m[1] = new Class_Auto("666", topliva, topliva_rasxod);
+                index = 1;
+                break;
+            case 3:
+                m[2] = new Class_Auto("555", topliva, topliva_rasxod);
+                index = 2;
+                break;
+        }
+
+        Class_Auto car = m[index];
+
+        while (true)
+        {
+            double speed = 60;
+            Console.WriteLine("\n");
+            Console.WriteLine($"При начальном скоросте {speed} км/ч - количество топлива в баке {topliva}л и  расход {topliva_rasxod} л/100км.");
+            Console.WriteLine("Увеличиваем скорость или тормозим? (для разгона введите положительное число, для торможения - отрицательное.)");
+            double speed_Chang = double.Parse(Console.ReadLine());
+
+            double Potreblenie = topliva_rasxod; // начальный расход
+
+            if (speed_Chang > 0)
+            {
+                // Увеличиваем скорость
+                while (speed < (60 + speed_Chang))
+                {
+                    speed += 1;
+                    Potreblenie += 0.1;
+                    Console.Clear();
+                    Console.WriteLine("Скорость: " + speed);
+                }
+            }
+            else if (speed_Chang < 0)
+            {
+                // Уменьшаем скорость
+                while (speed > (60 + speed_Chang))
+                {
+                    if (speed <= 1)
+                    {
+                        Console.WriteLine("Ошибка в торможении. Скорость не может быть меньше 1. Считаем, что вы ввели 0.");
+                        speed = 0.1;
+                        Potreblenie = 4;
+                        break;
+                    }
+                    else
+                    {
+                        speed -= 1;
+                        Potreblenie -= 0.1;
+                        Console.Clear();
+                        Console.WriteLine("Скорость: " + speed);
+                    }
+                }
+            }
+
+            Random random = new Random();
+            int rasstoyanie = random.Next(1, 100); // Случайное расстояние от 1 до 100 км
+            Console.WriteLine("Путь до места назначения составило: " + rasstoyanie + " km");
+
+            car.MOVE(rasstoyanie);
+
+            Console.WriteLine("Поездка завершена. Желаете продолжить? (да/нет)");
+            string continueTrip = Console.ReadLine();
+
+            car.Proidenei_put(rasstoyanie);
+
+            if (continueTrip.ToLower() == "нет")
+            {
+                Console.WriteLine("Общий пробег: " + car.Get_obshee_rasstoyanie() + " km");
+                break;
+            }
+
+        }
+    }
+}
